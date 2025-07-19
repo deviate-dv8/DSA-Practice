d29fea83483f0a27a8655bba57e7672cfc1e9707
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-07-19 19:11:51 +0800

    added better solution for no. 128

--- leetcode/128-longest-consecutive-sequence-[medium].py
+++ leetcode/128-longest-consecutive-sequence-[medium].py
@@ -1,21 +1,37 @@
 class Solution(object):
-    def longestConsecutive(self, nums):
-        if len(nums) == 0:
-            return 0
-        sortedNums = sorted(nums)
-        counter = 1
-        best = 1
-        for i in range(len(sortedNums) - 1):
-            if sortedNums[i] == sortedNums[i + 1]:
-                continue
-            if (sortedNums[i] - sortedNums[i + 1]) == -1:
-                counter += 1
-                if counter > best:
-                    best = counter
-            else:
-                counter = 1
-        return best
+    #     def longestConsecutive(self, nums):
+    #         if len(nums) == 0:
+    #             return 0
+    #         sortedNums = sorted(nums)
+    #         counter = 1
+    #         best = 1
+    #         for i in range(len(sortedNums) - 1):
+    #             if sortedNums[i] == sortedNums[i + 1]:
+    #                 continue
+    #             if (sortedNums[i] - sortedNums[i + 1]) == -1:
+    #                 counter += 1
+    #                 if counter > best:
+    #                     best = counter
+    #             else:
+    #                 counter = 1
+    #         return best
+    #
+    #         """
+    #         :type nums: List[int]
+    #         :rtype: int
+    #         """
 
+    def longestConsecutive(self, nums):
+        nonRepeatNums = set(nums)
+        best = 0
+        for num in nonRepeatNums:
+            if num - 1 not in nonRepeatNums:
+                increment = 0
+                while num + increment in nonRepeatNums:
+                    increment += 1
+                    if increment > best:
+                        best = increment
+        return best
         """
         :type nums: List[int]
         :rtype: int
